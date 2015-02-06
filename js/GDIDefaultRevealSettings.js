65c9684b16b5e6af2d36e261eea55d2571981565
ndurham12 <mrsnonadurham@gmail.com>
2015-02-05 23:59:21 -0600

    Updating Class3 and 4!

--- js/GDIDefaultRevealSettings.js
+++ js/GDIDefaultRevealSettings.js
@@ -18,15 +18,15 @@ Reveal.initialize({
     }, */
     // Optional libraries used to extend on reveal.js
     dependencies: [
-        { src: '~/reveal.js/lib/js/classList.js', condition: function () { return !document.body.classList; } },
-        { src: '~/reveal.js/plugin/markdown/showdown.js', condition: function () { return !!document.querySelector('[data-markdown]'); } },
-        { src: '~/reveal.js/plugin/markdown/markdown.js', condition: function () { return !!document.querySelector('[data-markdown]'); } },
-        { src: '~/reveal.js/plugin/highlight/highlight.js', async: true, condition: function () { return !!document.querySelector('pre code'); }, callback: function () { hljs.initHighlightingOnLoad(); } },
-        { src: '~/reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function () { return !!document.body.classList; } },
-        { src: '~/reveal.js/plugin/notes/notes.js', async: true, condition: function () { return !!document.body.classList; } },
+        { src: 'reveal.js/lib/js/classList.js', condition: function () { return !document.body.classList; } },
+        { src: 'reveal.js/plugin/markdown/showdown.js', condition: function () { return !!document.querySelector('[data-markdown]'); } },
+        { src: 'reveal.js/plugin/markdown/markdown.js', condition: function () { return !!document.querySelector('[data-markdown]'); } },
+        { src: 'reveal.js/plugin/highlight/highlight.js', async: true, condition: function () { return !!document.querySelector('pre code'); }, callback: function () { hljs.initHighlightingOnLoad(); } },
+        { src: 'reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function () { return !!document.body.classList; } },
+        { src: 'reveal.js/plugin/notes/notes.js', async: true, condition: function () { return !!document.body.classList; } },
         { src: 'http://cdnjs.cloudflare.com/ajax/libs/socket.io/0.9.16/socket.io.min.js', async: true },
-        { src: '~/reveal.js/plugin/multiplex/master.js', async: true },
-        { src: '~/reveal.js/plugin/multiplex/client.js', async: true }
+        { src: 'reveal.js/plugin/multiplex/master.js', async: true },
+        { src: 'reveal.js/plugin/multiplex/client.js', async: true }
 
     ]
 });
